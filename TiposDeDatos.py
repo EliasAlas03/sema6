000a78f2aa89758de41de6f77ee3e0518e30cfb4
EliasAlas03 <126420750+EliasAlas03@users.noreply.github.com>
2023-03-02 14:49:31 -0600

    Add files via upload

---
+++ TiposDeDatos.py
@@ -0,0 +1,26 @@
+#Tipos de datos enteros (int) y largos (Long)
+numeroEntero = 9
+print ("El valor de la variable NumeroEntero es:",numeroEntero, "y su tipo de dato es:", type (numeroEntero))
+numeroLargo = 9
+print ("El valor de la variable numeroLargoes: es",numeroLargo, "y su tipo de dato es:", type (numeroLargo))
+
+#Tipos de daros float
+numeroFlotante = 5.99
+print (numeroFlotante, type (numeroFlotante))
+
+#Tipo de datos Complejos
+numeroComplejo = 5 + 5j
+print (numeroComplejo, type (numeroComplejo))
+
+#Tipos de datos string
+nombre11 = "Aaron"
+nombre2 = "Geime"
+nombre3 = """Alicia
+Estefany
+Guzman
+Lopez"""
+nombre4 = "Edgar Rebe \n Rivas Castro"
+print (nombre1, type(nombre1))
+print (nombre2, type(nombre2))
+print (nombre3, type(nombre3))
+print (nombre4, type(nombre4))
